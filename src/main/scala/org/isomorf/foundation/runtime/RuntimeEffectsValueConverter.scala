6c93ef8548a7b790af6b5ff97c651221382c2377
bdkent <brian.kent@isomorf.io>
2018-06-18 17:44:36 -0400

    converting WeakTypeTag to ClassTag for eventual ScalaJS support

--- src/main/scala/org/isomorf/foundation/runtime/RuntimeEffectsValueConverter.scala
+++ src/main/scala/org/isomorf/foundation/runtime/RuntimeEffectsValueConverter.scala
@@ -1,13 +1,13 @@
 package org.isomorf.foundation.runtime
 
-import scala.reflect.runtime.universe._
+import scala.reflect.ClassTag
 
 /**
  * Handles the internal translations of values to/from Effects
  */
 trait RuntimeEffectsValueConverter {
 
-  def fromRTValueToEffectsValue[V, X](value: V)(implicit vTag: WeakTypeTag[V], xTag: WeakTypeTag[X]): Option[X]
+  def fromRTValueToEffectsValue[V, X](value: V)(implicit vTag: ClassTag[V], xTag: ClassTag[X]): Option[X]
 
-  def toRTValuefromEffectsValue[V, X](any: X)(implicit vTag: WeakTypeTag[V], xTag: WeakTypeTag[X]): Option[V]
+  def toRTValuefromEffectsValue[V, X](any: X)(implicit vTag: ClassTag[V], xTag: ClassTag[X]): Option[V]
 }
